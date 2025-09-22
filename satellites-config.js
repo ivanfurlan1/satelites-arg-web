5e875081340d1282e92d05c222629679b597f15d
ivanfurlan1 <66447232+ivanfurlan1@users.noreply.github.com>
2025-09-22 05:22:44 -0300

    Actualizar satellites-config.js

--- satellites-config.js
+++ satellites-config.js
@@ -32,7 +32,7 @@ const SATELLITES_CONFIG = {
     // Lista de los últimos Starlinks (actualizar manualmente cuando sea necesario)
     latestStarlinks: [
         {
-            tle: `TARLINK-G10-27 SINGLE  
+            tle: `STARLINK-G10-27 SINGLE  
 1 72001C 25212B   25264.49808148  .01097600  00000+0  17662-2 0    02
 2 72001  53.1624  60.8392 0010969 267.8465   7.8625 16.02316865    11`
         },
